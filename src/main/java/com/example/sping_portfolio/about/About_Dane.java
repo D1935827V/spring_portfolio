d97a93edcbbeb120d19df2e336aa1c32d28fffbd
D1935827V <dvestal2102@gmail.com>
2021-09-01 10:00:16 -0700

    Abouts Directory Added for HTML

--- src/main/java/com/example/sping_portfolio/about/About_Dane.java
+++ src/main/java/com/example/sping_portfolio/about/About_Dane.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class About_Dane {
-    @GetMapping("/Dane")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    @GetMapping("Abouts/Dane")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String abtDane(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         model.addAttribute("name", name); // MODEL is passed to html
-        return "Dane"; // returns HTML VIEW (greeting)
+        return "Abouts/Dane"; // returns HTML VIEW (greeting)
     }
 }
