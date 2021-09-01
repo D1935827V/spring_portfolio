cafc752e74a9af1f95f02967b91a84692475cd7c
D1935827V <dvestal2102@gmail.com>
2021-09-01 10:03:52 -0700

    Abouts Directory Added for HTML for everyone

--- src/main/java/com/example/sping_portfolio/about/About_Calvin.java
+++ src/main/java/com/example/sping_portfolio/about/About_Calvin.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class About_Calvin {
-    @GetMapping("/Calvin")
+    @GetMapping("Abouts/Calvin")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String birds() {
-        return "Calvin"; // returns HTML VIEW (greeting)
+        return "Abouts/Calvin"; // returns HTML VIEW (greeting)
     }
 }
 
--- src/main/java/com/example/sping_portfolio/about/About_Nicholas.java
+++ src/main/java/com/example/sping_portfolio/about/About_Nicholas.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class About_Nicholas {
-    @GetMapping("/Nicholas")
+    @GetMapping("Abouts/Nicholas")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
     public String Nicholas() {
-        return "Nicholas"; // returns HTML VIEW (greeting)
+        return "Abouts/Nicholas"; // returns HTML VIEW (greeting)
     }
 }
 
