67815301ed26b69994cf9ae61570af9ee02cb438
ncrxue <nxue2004@gmail.com>
2021-09-01 09:57:19 -0700

    Created calculateAverage challenge

---
+++ src/main/java/com/example/sping_portfolio/controllers/calculateAverage.java
@@ -0,0 +1,19 @@
+package com.example.sping_portfolio.controllers;
+
+public class calculateAverage {
+    public static void main(String[] args) {
+        double grade1 = 93;
+        double grade2 = 88;
+        double grade3 = 91;
+        double grade4 = 94;
+        double grade5 = 87;
+        double avg12 = (grade1 + grade2)/2;
+        double avg34 = (grade3 + grade4)/2;
+        double finalAvg = (grade1 + grade2 + grade3 + grade4 + grade5)/5;
+        System.out.println("Grading Period 1: " + avg12);
+        System.out.println("Grading Period 2: " + avg34);
+        System.out.println("Final: " + grade5);
+        System.out.println("Average: " + finalAvg);
+    }
+}
+/* Written by Nicholas */
