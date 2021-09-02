5165b353c70520e1d0765cb81687c461fd4adb89
BraydenBasinger <braydenbasinger@gmail.com>
2021-09-02 10:24:37 -0700

    finished about me page

--- src/main/java/com/example/sping_portfolio/about/About_Brayden.java
+++ src/main/java/com/example/sping_portfolio/about/About_Brayden.java
@@ -1,4 +1,8 @@
 package com.example.sping_portfolio.about;
 
 public class About_Brayden {
+    public static void main(String [] args) {
+        System.out.println("i have no idea what im doing");
+    }
+// i legit have no idea what im doing
 }
