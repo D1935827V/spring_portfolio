28327dcaec98bb5e3db4eb5e5dc6cd8304586820
BraydenBasinger <braydenbasinger@gmail.com>
2021-09-02 10:32:36 -0700

    updated about me page

--- src/main/java/com/example/sping_portfolio/about/About_Brayden.java
+++ src/main/java/com/example/sping_portfolio/about/About_Brayden.java
@@ -1,8 +1,13 @@
 package com.example.sping_portfolio.about;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class About_Brayden {
-    public static void main(String [] args) {
-        System.out.println("i have no idea what im doing");
+    @GetMapping("Abouts/Brayden")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String birds() {
+        return "Abouts/Brayden"; // returns HTML VIEW (greeting)
     }
-// i legit have no idea what im doing
 }
