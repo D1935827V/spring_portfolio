e791db7eb456409571a5e92d6338e5b101237929
ncrxue <nxue2004@gmail.com>
2021-09-09 12:59:22 -0700

    Fixed About Us links and added navbar to top of Nicholas About Me page

--- src/main/java/com/example/sping_portfolio/about/About_Nicholas.java
+++ src/main/java/com/example/sping_portfolio/about/About_Nicholas.java
@@ -7,10 +7,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class About_Nicholas {
-    @GetMapping("Abouts/Nicholas")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String abtDane(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
-        model.addAttribute("name", name); // MODEL is passed to html
-        return "Abouts/Nicholas"; // returns HTML VIEW (greeting)
+    @GetMapping("Abouts/Nicholas")
+    public String AboutNicholas() {
+        return "Abouts/Nicholas";
     }
 }
