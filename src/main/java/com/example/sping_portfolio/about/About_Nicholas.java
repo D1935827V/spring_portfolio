56467415f860871db8ec985b6a75af4eb7a7bb7a
ncrxue <nxue2004@gmail.com>
2021-09-09 09:52:19 -0700

    Added HTML and images for Mini Lab 2

--- src/main/java/com/example/sping_portfolio/about/About_Nicholas.java
+++ src/main/java/com/example/sping_portfolio/about/About_Nicholas.java
@@ -1,14 +1,16 @@
 package com.example.sping_portfolio.about;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+@Controller
 public class About_Nicholas {
-    @GetMapping("Abouts/Nicholas")
-    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String Nicholas() {
+    @GetMapping("Abouts/Nicholas")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String abtDane(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+        model.addAttribute("name", name); // MODEL is passed to html
         return "Abouts/Nicholas"; // returns HTML VIEW (greeting)
     }
 }
-
