eb8b70b8125a06dff81380feebbb68d039c81716
ncrxue <nxue2004@gmail.com>
2021-09-21 11:15:20 -0700

    Prototype palindrome program, TODO add it as a page to site

---
+++ src/main/java/com/example/sping_portfolio/Palindrome/Palindrome.java
@@ -0,0 +1,26 @@
+package com.example.sping_portfolio.Palindrome;
+
+public class Palindrome {
+    static boolean isPalindrome(String str)
+    {
+        int i = 0, j = str.length() - 1;
+
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        String str = "neveroddoreven";
+
+        if (isPalindrome(str))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+    }
+}
