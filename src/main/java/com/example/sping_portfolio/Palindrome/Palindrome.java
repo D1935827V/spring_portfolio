cfc731aaca69c44bf4d0f638cff170e23eff3bd6
ncrxue <nxue2004@gmail.com>
2021-09-24 10:36:17 -0700

    For loop implementation of palindrome

--- src/main/java/com/example/sping_portfolio/Palindrome/Palindrome.java
+++ src/main/java/com/example/sping_portfolio/Palindrome/Palindrome.java
@@ -9,7 +9,8 @@ public class Palindrome {
     {
         str = str.replaceAll("\\s", "");
         str = str.toLowerCase(Locale.ROOT);
-        int i = 0, j = str.length() - 1;
+        int i = 0;
+        int j = str.length() - 1;
 
         while (i < j) {
             if (str.charAt(i) != str.charAt(j))
@@ -19,4 +20,34 @@ public class Palindrome {
         }
         return "The phrase is a palindrome";
     }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man A plan A canal Panama"));
+    }
 }
+
+/* Implementation of Palindrome Checker using a For loop
+
+package com.example.sping_portfolio.Palindrome;
+
+import java.util.Locale;
+
+public class PalindromeFor {
+    public static String isPalindromeFor(String str)
+    {
+        str = str.replaceAll("\\s", "");
+        str = str.toLowerCase(Locale.ROOT);
+
+        for (int i = 0, j = str.length() - 1; i < j; i++, j--) {
+            if (str.charAt(i) != str.charAt(j))
+                return "The phrase is not a palindrome.";
+        }
+        return "The phrase is a palindrome";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindromeFor("A man A plan A canal Panama"));
+    }
+}
+
+*/
