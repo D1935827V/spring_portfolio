9201d0f3ef993b5ea96a14d715f9c22bf85bf4e0
BraydenBasinger <braydenbasinger@gmail.com>
2021-10-15 14:44:58 -0700

    added ascii art

---
+++ src/main/java/com/example/sping_portfolio/Ascii_Brayden/Asciiart.java
@@ -0,0 +1,28 @@
+package com.example.sping_portfolio.Ascii_Brayden;
+
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+
+public class Asciiart {
+        public static void main(String[] args) {
+            int width = 150;
+            int height = 30;
+            BufferedImage image = new BufferedImage (width, height, BufferedImage.TYPE_INT_RGB);
+            Graphics g = image.getGraphics();
+            g.setFont(new Font ("SansSerif", Font.BOLD, 24));
+            Graphics2D g2 = (Graphics2D) g;
+            g2 .setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+            g2.drawString("ADHITHI", 10, 20);
+            for (int y = 0; y < height; y++) {
+                StringBuilder builder = new StringBuilder();
+                for (int x = 0; x < width; x++) {
+                    builder.append(image.getRGB(x,y) == -16777216 ? "@" : "");
+                }
+            }
+        }
+    }
+
+
