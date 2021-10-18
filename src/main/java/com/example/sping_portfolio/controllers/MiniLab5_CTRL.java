4701d17412c180f29bdc616151f2b7c08b436bc6
D1935827V <dvestal2102@gmail.com>
2021-10-18 09:36:29 -0700

    remove amogus

--- src/main/java/com/example/sping_portfolio/controllers/MiniLab5_CTRL.java
+++ src/main/java/com/example/sping_portfolio/controllers/MiniLab5_CTRL.java
@@ -17,30 +17,17 @@ public class MiniLab5_CTRL {
         String web_server = "http://localhost:8080";
         List<ImageInfo> lii = new ArrayList<>();
 
-        ArrayList<String> list = new ArrayList<String>();
-        list.add("/images/whereisthetwix.png");
-        list.add("/images/calvinbackhurts.png");
-        list.add("/images/Mona_Lisa.png");
-
-        Iterator iterator = list.iterator();
-        int i=0;
-        while(iterator.hasNext()) {
-            lii.add(new ImageInfo(iterator.toString(), web_server + (iterator.next()), 12));
-            lii.get(i).read_image();
-            i++;
-        }
-
-        /*String file0 = "/images/Mona_Lisa_RE.png";
+       String file0 = "/images/Mona_Lisa_RE.png";
         lii.add(new ImageInfo(file0, web_server + file0, 12));
         lii.get(0).read_image();
 
         String file1 = "/images/calvinbackhurts.png";
-        lii.add(new ImageInfo(file1, web_server + file1, 2));
+        lii.add(new ImageInfo(file1, web_server + file1, 12));
         lii.get(1).read_image();
 
         String file2 = "/images/whereisthetwix.png";
         lii.add(new ImageInfo(file2, web_server + file2, 12));
-        lii.get(2).read_image();*/
+        lii.get(2).read_image();
 
         model.addAttribute("lii", lii);
         return "/MiniLab5/MiniLab5";
@@ -63,18 +50,6 @@ public class MiniLab5_CTRL {
             lii.get(i).read_image();
             i++;
         }
-
-        /*String file0 = "/images/whereisthetwix.png";
-        lii.add(new ImageInfo(file0, web_server + file0, 12));
-        lii.get(0).read_image();
-
-        String file1 = "/images/calvinbackhurts.png";
-        lii.add(new ImageInfo(file1, web_server + file1, 12));
-        lii.get(1).read_image();
-
-        String file2 = "/images/Mona_Lisa.png";
-        lii.add(new ImageInfo(file2, web_server + file2, 12));
-        lii.get(2).read_image();*/
 
         model.addAttribute("lii", lii);
         return "/MiniLab5/image_grayscale";
